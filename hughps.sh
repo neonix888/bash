7ee82617cd0e5fa4d12d5e01a2ba29cf11845da2
Hugh Nguyen <lpr@pobox.com>
2023-04-25 10:00:10 -0700

    Updated on hughps.sh

--- hughps.sh
+++ hughps.sh
@@ -4,7 +4,7 @@
 if [ -z "$1" ]
 then
   echo "Error: Please provide the PID of the process to monitor as a command-line argument"
-  echo "Usage: ./script.sh <PID>"
+  echo "Usage: $0 <PID>"
   exit 1
 fi
 
@@ -15,7 +15,7 @@ PID=$1
 DURATION=$((30*60))
 
 # Specify the delay between updates (in seconds)
-DELAY=5
+DELAY=1
 
 # Output file for statistics
 OUTPUT_FILE="process_stats.txt"
@@ -29,11 +29,18 @@ for ((i=0; i<$DURATION; i+=$DELAY)); do
     TIME=$(date +%H:%M:%S)
 
     # Run top command and extract CPU, memory, and disk I/O statistics for specified process
-    TOP_OUTPUT=$(top -b -n1 -p $PID | grep $PID)
-    CPU=$(echo $TOP_OUTPUT | awk '{print $9}')
-    MEM=$(echo $TOP_OUTPUT | awk '{print $10}')
-    DISK_READ=$(echo $TOP_OUTPUT | awk '{print $6}')
-    DISK_WRITE=$(echo $TOP_OUTPUT | awk '{print $7}')
+    TOP_OUTPUT=$(top -b -n1 -p $PID | awk -v pid=$PID '$1==pid {print $9, $10, $6, $7}')
+
+    if [ -z "$TOP_OUTPUT" ]; then
+        echo "Error: Process with PID $PID not found."
+        exit 1
+    fi
+
+    # Extract individual statistics
+    CPU=$(echo $TOP_OUTPUT | awk '{print $1}')
+    MEM=$(echo $TOP_OUTPUT | awk '{print $2}')
+    DISK_READ=$(echo $TOP_OUTPUT | awk '{print $3}')
+    DISK_WRITE=$(echo $TOP_OUTPUT | awk '{print $4}')
 
     # Append statistics to output file
     echo "$TIME, $CPU, $MEM, $DISK_READ, $DISK_WRITE" >> $OUTPUT_FILE
@@ -41,3 +48,4 @@ for ((i=0; i<$DURATION; i+=$DELAY)); do
     # Wait for specified delay before collecting next set of statistics
     sleep $DELAY
 done
+
